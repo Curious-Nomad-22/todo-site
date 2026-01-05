748f9fb17c268c7be5eb333cd0dec190dc513989
Sangeetha A <145581501+Curious-Nomad-22@users.noreply.github.com>
2026-01-05 23:12:34 +0530

    Update API base URL for login functionality

--- frontend/login.js
+++ frontend/login.js
@@ -1,4 +1,4 @@
-const API_BASE_URL= "https://todo-frontend-p05u.onrender.com";
+const API_BASE_URL= "https://todo-site-1ql4.onrender.com";
 
 
 document.getElementById("loginForm").addEventListener("submit", async (e) => {
