b89501302493df13551e6b702179fe42b6ca9e5f
curious_nomad <sangeethaarangaraj22@gmail.com>
2026-01-05 21:21:11 +0530

    Update Everything

--- auth/database.py
+++ auth/database.py
@@ -3,7 +3,7 @@ from sqlmodel import create_engine
 
 
 
-sqlite_url = f"sqlite:///auth/base.db"
+sqlite_url = f"sqlite:///base.db"
 
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, connect_args=connect_args)
