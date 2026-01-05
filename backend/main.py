7e8f2a4aeea55a72aa189785a2055410c00fd158
Sangeetha A <145581501+Curious-Nomad-22@users.noreply.github.com>
2026-01-05 22:42:12 +0530

    Update CORS settings to allow all origins

--- backend/main.py
+++ backend/main.py
@@ -15,9 +15,7 @@ app.include_router(log.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[ "http://localhost:3000", 
-         "http://127.0.0.1:5500",
-         "http://localhost:5500",],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
