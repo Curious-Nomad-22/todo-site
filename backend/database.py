661626db48ca413454ab81f75ecefe8bdcf98573
Sangeetha A <145581501+Curious-Nomad-22@users.noreply.github.com>
2026-01-06 14:00:47 +0530

    Refactor database connection logic with environment variable

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,12 @@
 from sqlmodel import create_engine
+import os
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-
-
-
-sqlite_url = f"sqlite:///base.db"
-
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
+if DATABASE_URL:
+  engine=create_engine(DATABASE_URL)
+else:
+  sqlite_url = f"sqlite:///base.db"
+  connect_args = {"check_same_thread": False}
+  engine = create_engine(sqlite_url, connect_args=connect_args)
 
 
